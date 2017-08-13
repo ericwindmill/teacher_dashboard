f78070c23f68b73bec064f512d6d20c212c1b082
Eric Windmill <ericwindmill@gmail.com>
2017-08-13 14:20:03 -0700

    View responses in place

--- src/components/assignment/SubmissionDetail.vue
+++ src/components/assignment/SubmissionDetail.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class='StudentSubmission'>
+  <div class='StudentSubmission'
+    @click='changeShow(submission.id)'
+  >
     <dt>
       <img :src="submission.creator.avatars.small" />
       <h3>{{submission.creator.first_name}} {{submission.creator.last_name}}</h3>
       <sub>Username: {{submission.creator.username}}</sub>
       <sub>ID: {{submission.creator.id}}</sub>
-      <icon v-show="complete" class='QuestionComplete' name='check-circle'></icon>
-      <icon v-show='!complete' class='QuestionIncomplete' name='circle'></icon>
+      <icon v-show="submission.status !== 'not_graded'" class='QuestionComplete' name='check-circle'></icon>
+      <icon v-show="submission.status === 'not_graded'" class='QuestionIncomplete' name='circle'></icon>
     </dt>
-    <dd>
+    <dd v-show='submission.id === show'>
       {{submission.content}}
     </dd>
   </div>
@@ -16,19 +18,35 @@
 
 <script>
 export default {
-  data() {
-    return {
-      complete: false
-    }
-  },
   props: {
     submission: {
       type: Object
+    },
+    graded: {
+      type: String
+    }
+  },
+  data() {
+    return {
+      show: false
+    }
+  },
+  methods: {
+    changeShow(id) {
+      if (this.show === id) {
+        this.show = false
+      } else {
+        this.show = id
+      }
     }
   }
 }
 </script>
 
-<style>
+<style lang='scss'>
+@import "../../assets/styles/_vars.scss";
+.StudentSubmission {
+  border: 1px solid $grey-light;
+}
   
 </style>
